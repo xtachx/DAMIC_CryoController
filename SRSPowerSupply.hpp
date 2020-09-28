1a04cfa0eb50acac6a50cbe7199ac43792a88085
Pitam Mitra <pitamm@uw.edu>
2020-09-28 15:43:02 -0700

    Added SRS code

---
+++ SRSPowerSupply.hpp
@@ -0,0 +1,65 @@
+/* **************************************************************************************
+ * Code containing the Routines required to communicate and operate the SRS Power Supply
+ * Module. 
+ *
+ * by Pitam Mitra 2020 for DAMIC-M
+ * **************************************************************************************/
+
+
+#ifndef SRS_HPP_INCLUDED
+#define SRS_HPP_INCLUDED
+
+
+/*Includes*/
+#include <iostream>
+#include "SerialDeviceT.hpp"
+
+class SRSPowerSupply : public SerialDevice {
+public:
+    SRSPowerSupply(std::string );
+    ~SRSPowerSupply();
+    void UpdateMysql(void);
+
+    /*SRS Related Routines*/
+
+    //Get Parameters
+    template <typename T>
+    T GetParameterFromSRS(std::string );
+    //Set Parameters
+
+    // Get PS Values
+	double ReadPSVoltage();
+	bool ReadPSOutput();
+    std::string IDN();
+    bool IsOVLD(void);
+
+	// Set PS Values
+	void WritePSVoltage(double voltage);
+	void WritePSOutput(bool output);
+
+    //Ramp gen
+	void VoltageRamp(double startScanVoltage, double stopScanVoltage, double scanTime, bool display);
+
+
+private:
+
+	float currentVoltage;
+	bool currentOutputStatus;
+    bool OVLDStatus;
+
+
+    bool WatchdogFuse;
+    bool SRSPowerState;
+    std::string SQLStatusMsg;
+
+
+
+    int _cHeaterMode, _cWatchdogFuse;
+
+
+};
+
+
+
+
+#endif
